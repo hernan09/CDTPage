7793738f4dabac154d6907f703a8534c41806ad5
hernan <hernag_09@hotmail.com>
2019-03-31 16:51:32 -0200

    first commit

---
+++ build/precache-manifest.1033dfa565c8cd378801afe867dd96a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b31ed1f3886e9bcea872",
+    "url": "/static/js/main.2c4347ce.chunk.js"
+  },
+  {
+    "revision": "758c1e8a81b7cfa8c83a",
+    "url": "/static/js/2.1bfa6c19.chunk.js"
+  },
+  {
+    "revision": "b31ed1f3886e9bcea872",
+    "url": "/static/css/main.d6f52315.chunk.css"
+  },
+  {
+    "revision": "e285a4ef0135a4438b64e98d533e040d",
+    "url": "/index.html"
+  }
+];
